98d4a7f8ad05fd449132bb1591d7df810fa24641
Mohamedbbahaa <mohamedbbahaa404@gmail.com>
2024-10-21 00:18:32 +0300

    bookstore

---
+++ alx_book_store.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id FOREIGN KEY REFERENCES Authors(author_id),
+    price DOUBLE NOT NULL,
+    publication_date DATE
+);
+
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    adress TEXT
+);
+
+CREATE TABLE Orders(
+    order_id PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT FOREIGN KEY REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
